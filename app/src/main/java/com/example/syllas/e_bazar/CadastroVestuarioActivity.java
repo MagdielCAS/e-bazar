5bb3e50e9b4e2cfb6be5708448346bd04d09ce86
Syllas Rangel <syllasrangel@hotmail.com>
2016-04-17 02:14:11 -0300

    Chamar camera implementado

--- app/src/main/java/com/example/syllas/e_bazar/CadastroVestuarioActivity.java
+++ app/src/main/java/com/example/syllas/e_bazar/CadastroVestuarioActivity.java
@@ -1,7 +1,9 @@
 package com.example.syllas.e_bazar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CadastroVestuarioActivity extends AppCompatActivity {
 
@@ -10,4 +12,20 @@ public class CadastroVestuarioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_vestuario);
     }
+
+    public void tirarFoto(View view){
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivityForResult(intent,0);
+    }
+
+    @Override
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(data != null){
+            Bundle bundle = data.getExtras();
+            //Continuar aqui...
+        }
+    }
+
 }
