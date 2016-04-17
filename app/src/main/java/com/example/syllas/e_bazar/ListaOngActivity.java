4c1ceb24c3414df6203c6e937f9b6a762db19d93
Magdiel <magdielc.a.sousa@alu.ufc.br>
2016-04-17 00:51:43 -0300

    menu arrumado

--- app/src/main/java/com/example/syllas/e_bazar/ListaOngActivity.java
+++ app/src/main/java/com/example/syllas/e_bazar/ListaOngActivity.java
@@ -2,8 +2,6 @@ package com.example.syllas.e_bazar;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -71,7 +69,7 @@ public class ListaOngActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.lista_ong, menu);
+        getMenuInflater().inflate(R.menu.menu_drawer, menu);
         return true;
     }
 
