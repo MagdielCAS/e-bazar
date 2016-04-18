fe415457b403bccb477db3019a4da198fe342cfb
MagdielCAS <magdielc.a.sousa@alu.ufc.br>
2016-04-18 13:05:50 -0300

    adicionando carrinho

--- app/src/main/java/com/example/syllas/e_bazar/CarrinhoActivity.java
+++ app/src/main/java/com/example/syllas/e_bazar/CarrinhoActivity.java
@@ -1,12 +1,20 @@
 package com.example.syllas.e_bazar;
 
+import android.content.Intent;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 
 public class CarrinhoActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+
+    private EbazarDAO ebazarDAO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,10 +22,74 @@ public class CarrinhoActivity extends AppCompatActivity implements NavigationVie
         setContentView(R.layout.activity_carrinho);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ebazarDAO = new EbazarDAO(this);
     }
 
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        return false;
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_drawer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId(); //item do menu
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.cadastroVest) {
+            startActivity(new Intent(this, CadastroVestuarioActivity.class));
+        } else if (id == R.id.cadastroOng) {
+
+        } else if (id == R.id.listOng) {
+            startActivity(new Intent(this, ListaOngActivity.class));
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    protected void onDestroy() {
+        ebazarDAO.close();
+        super.onDestroy();
+    }
+
 }
